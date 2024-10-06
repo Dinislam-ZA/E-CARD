df5b46b3e9dfe6de05c14cd302c228ff96c65355
Dinislam Zalimkhanov <Zalimkhanova04@mail.ru>
2024-10-07 02:57:23 +0300

    Android | FAB changed to top bar button

--- ECardAndroid/app/src/main/java/com/example/e_card_android/MainActivity.kt
+++ ECardAndroid/app/src/main/java/com/example/e_card_android/MainActivity.kt
@@ -6,12 +6,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.DrawerValue
-import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.navigation.compose.rememberNavController
@@ -21,6 +23,7 @@ import com.example.e_card_android.ui.theme.ECardAndroidTheme
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -34,15 +37,20 @@ class MainActivity : ComponentActivity() {
                     drawerState = drawerState
                 ) {
                     Scaffold(
-                        floatingActionButton = {
-                            ExtendedFloatingActionButton(
-                                text = { Text("Show drawer") },
-                                icon = { Icon(Icons.Filled.Add, contentDescription = "") },
-                                onClick = {
-                                    scope.launch {
-                                        drawerState.apply {
-                                            if (isClosed) open() else close()
+                        topBar = {
+                            TopAppBar(
+                                title = { Text("E-Card Game") },
+                                navigationIcon = {
+                                    IconButton(onClick = {
+                                        scope.launch {
+                                            if (drawerState.isClosed) {
+                                                drawerState.open()
+                                            } else {
+                                                drawerState.close()
+                                            }
                                         }
+                                    }) {
+                                        Icon(Icons.Filled.Menu, contentDescription = "Open Drawer")
                                     }
                                 }
                             )
